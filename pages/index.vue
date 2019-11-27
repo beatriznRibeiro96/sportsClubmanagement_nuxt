19e55ae497e09fdb6ab4cd261105dbee12c7c013
Beatriz Ribeiro <2160796@my.ipleiria.pt>
2019-11-27 00:22:59 +0000

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- easy components usage, already shipped with bootstrap css-->
+  <b-container>
+    <!-- try to remove :fields=”fields” to see the magic -->
+    <b-table striped over :items="administrators" :fields="fields" />
+  </b-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fields: ["username", "name", "email"],
+      administrators: []
+    };
+  },
+  created() {
+    this.$axios.$get("http://localhost:8080/SportsClubManagement_war_exploded/api/administrators").then(administrators => {
+      this.administrators = administrators;
+    });
+  }
+};
+</script>
+<style>
+</style>
