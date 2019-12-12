e94061a44edc68c555aa04de5186cb289a950d53
Luís Pimentel <2160863@my.ipleiria.pt>
2019-12-12 11:13:30 +0000

    Login e logout
    Páginas de login e botão de logout.
    Página da lista de administradores só é mostrada para administradores

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <nuxt />
+    <b-container fluid>
+      <b-row>
+        <b-col md="3" class="m1-md-auto p-1">
+          <b-button @click.prevent="logout" v-if="$auth.loggedIn">Logout</b-button>
+        </b-col>
+      </b-row>
+      <nuxt />
+    </b-container>
   </div>
 </template>
+<script>
+    export default {
+        methods: {
+            logout() {
+                this.$auth.logout('local')
+            }
+        }
+    }
+</script>
 <style>
 </style>
