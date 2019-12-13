50a6e2425865eb49e5dd39ef3804bb125319d021
beatriz <2160796@my.ipleiria.pt>
2019-12-13 15:04:15 +0000

    crud atleta

---
+++ pages/athletes/_username.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container>
+    <h3>Athlete Details</h3>
+    <p>Username: {{ athlete.username }}</p>
+    <p>Name: {{ athlete.name }}</p>
+    <p>Email: {{ athlete.email }}</p>
+    
+    
+    <b-btn variant="secondary" to="/athletes">Back</b-btn>
+  </b-container>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                athlete: {}
+                
+            }
+        },
+        computed: {
+            username() {
+                return this.$route.params.username
+            },
+            sports() {
+                return this.athlete.sports || []
+            }
+        },
+        created() {
+            this.$axios.$get(`/api/athletes/${this.username}`)
+                .then(coach => this.athlete = coach || {})
+        },
+    }
+</script>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,6 @@
     <b-btn variant="dark" to="/coaches">Coaches</b-btn>
     <b-btn variant="dark" to="/partners">Partners</b-btn>
     <b-btn variant="dark" to="/sports">Sports</b-btn>
+    <b-btn variant="dark" to="/athletes">Athletes</b-btn>
   </b-container>
 </template>
