c92950f43fae8f866590e7e21c3796dc64ee1ced
Luís Pimentel <2160863@my.ipleiria.pt>
2019-12-20 16:55:18 +0000

    Modalides sem id
    Modalidades, Modalidades Lecionadas e Inscrições não precisam de código para serem criadas, pois o mesmo é criado automaticamente

--- pages/activeSports/create.vue
+++ pages/activeSports/create.vue
@@ -2,14 +2,6 @@
   <b-container>
     <h3>Create new Active Sport</h3>
     <b-form @submit.prevent="create">
-      <b-form-group label="Code" description="Enter a code">
-        <b-input
-          name="code"
-          type="number"
-          placeholder="Code"
-          v-model.trim="code"
-          required />
-      </b-form-group>
       <b-form-group label="Name" description="Enter a name">
         <b-input
           name="name"
@@ -50,7 +42,6 @@
     export default {
         data() {
             return {
-                code: null,
                 name: null,
                 sportCode: null,
                 sports: [],
@@ -65,7 +56,6 @@
         methods: {
             create() {
                 this.$axios.$post('/api/activeSports', {
-                    code: this.code,
                     name: this.name,
                     sportCode: this.sportCode,
                     seasonCode: this.seasonCode
@@ -74,7 +64,6 @@
                     })
             },
             reset(){
-                this.code = null
                 this.name = null
                 this.sportCode = null
                 this.seasonCode = null
--- pages/sports/create.vue
+++ pages/sports/create.vue
@@ -2,14 +2,6 @@
   <b-container>
     <h3>Create new Sport</h3>
     <b-form @submit.prevent="create">
-      <b-form-group label="Code" description="Enter a code">
-        <b-input
-          name="code"
-          type="number"
-          placeholder="Code"
-          v-model.trim="code"
-          required />
-      </b-form-group>
       <b-form-group label="Name" description="Enter a name">
         <b-input
           name="name"
@@ -28,21 +20,18 @@
     export default {
         data() {
             return {
-                code: null,
                 name: null
             }
         },
         methods: {
             create() {
                 this.$axios.$post('/api/sports', {
-                    code: this.code,
                     name: this.name
                 })
                     .then(() => {this.$router.back()
                     })
             },
             reset(){
-                this.code = null
                 this.name = null
             }
         }
