8985f1b61e86476ed8810fa504c4cd78615e929e
Luís Pimentel <2160863@my.ipleiria.pt>
2019-12-23 01:19:45 +0000

    Tabelas com filtros e sort

--- pages/administrators/index.vue
+++ pages/administrators/index.vue
@@ -1,7 +1,47 @@
 <template>
   <div>
     <b-container>
-      <b-table striped over :items="administrators" :fields="fields">
+      <b-row>
+        <b-col lg="6" class="my-1">
+          <b-form-group
+            label="Filter"
+            label-cols-sm="3"
+            label-align-sm="right"
+            label-size="sm"
+            label-for="filterInput"
+            class="mb-0"
+          >
+            <b-input-group size="sm">
+              <b-form-input
+                v-model="filter"
+                type="search"
+                id="filterInput"
+                placeholder="Type to Search"
+              ></b-form-input>
+              <b-input-group-append>
+                <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+
+        <b-col lg="6" class="my-1">
+          <b-form-group
+            label="Filter On"
+            label-cols-sm="3"
+            label-align-sm="right"
+            label-size="sm"
+            description="Leave all unchecked to filter on all data"
+            class="mb-0">
+            <b-form-checkbox-group v-model="filterOn" class="mt-1">
+              <b-form-checkbox value="username">Username</b-form-checkbox>
+              <b-form-checkbox value="name">Name</b-form-checkbox>
+              <b-form-checkbox value="email">Email</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-table bordered hover head-variant="dark" :items="administrators" :fields="fields" :filter="filter" :filterIncludedFields="filterOn" sort-icon-left>
         <template v-slot:cell(actions)="row">
           <b-btn variant="info"
                  :to="`/administrators/${row.item.username}`">DETAILS</b-btn>
@@ -22,8 +62,15 @@
 export default {
   data() {
     return {
-      fields: ["username", "name", "email", 'actions'],
-      administrators: []
+      fields: [
+          {key: "username", label: "Username", sortable: true},
+          {key: "name", label: "Name", sortable: true},
+          {key: "email", label: "Email", sortable: true},
+          {key: "actions", label: "Actions"}
+      ],
+      administrators: [],
+      filter: null,
+      filterOn: []
     };
   },
     created () {
--- pages/sportSubscriptions/index.vue
+++ pages/sportSubscriptions/index.vue
@@ -1,7 +1,48 @@
 <template>
   <div>
     <b-container>
-      <b-table striped over :items="sportSubscriptions" :fields="fields">
+      <b-row>
+        <b-col lg="6" class="my-1">
+          <b-form-group
+            label="Filter"
+            label-cols-sm="3"
+            label-align-sm="right"
+            label-size="sm"
+            label-for="filterInput"
+            class="mb-0"
+          >
+            <b-input-group size="sm">
+              <b-form-input
+                v-model="filter"
+                type="search"
+                id="filterInput"
+                placeholder="Type to Search"
+              ></b-form-input>
+              <b-input-group-append>
+                <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+
+        <b-col lg="6" class="my-1">
+          <b-form-group
+            label="Filter On"
+            label-cols-sm="3"
+            label-align-sm="right"
+            label-size="sm"
+            description="Leave all unchecked to filter on all data"
+            class="mb-0">
+            <b-form-checkbox-group v-model="filterOn" class="mt-1">
+              <b-form-checkbox value="code">Code</b-form-checkbox>
+              <b-form-checkbox value="name">Name</b-form-checkbox>
+              <b-form-checkbox value="activeSportName">Active Sport Name</b-form-checkbox>
+              <b-form-checkbox value="athleteName">Athlete Name</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-table bordered hover head-variant="dark" :items="sportSubscriptions" :fields="fields" :filter="filter" :filterIncludedFields="filterOn" sort-icon-left>
         <template v-slot:cell(actions)="row">
           <b-btn variant="info"
                  :to="`/sportSubscriptions/${row.item.code}`">DETAILS</b-btn>
@@ -22,8 +63,16 @@
     export default {
         data () {
             return {
-                fields: ['code', 'name', 'activeSportName', 'athleteName', 'actions'],
-                sportSubscriptions: []
+                fields: [
+                    {key: "code", label: "Code", sortable: true},
+                    {key: "name", label: "Name", sortable: true},
+                    {key: "activeSportName", label: "Active Sport Name", sortable: true},
+                    {key: "athleteName", label: "Athlete Name", sortable: true},
+                    {key: "actions", label: "Actions"}
+                ],
+                sportSubscriptions: [],
+                filter: null,
+                filterOn: []
             }
         },
         created () {
